3483181a19569e12f41e89b8fd228c3f82e89a66
Max van Essen <max.vessen@icloud.com>
2023-02-16 02:31:04 +0100

    Add several icon fixes

--- src/ui/ZenView.ts
+++ src/ui/ZenView.ts
@@ -1,10 +1,12 @@
-import {setIcon, View, WorkspaceLeaf} from "obsidian";
+import {getIcon, Menu, setIcon, View, WorkspaceLeaf} from "obsidian";
 import {VIEW_TYPE_ZEN} from "../constants";
 import Zen from "../main";
 
 
 export class ZenView extends View {
 	leaf: WorkspaceLeaf;
+
+	headerIcon: HTMLElement;
 
 	plugin: Zen;
 
@@ -14,34 +16,72 @@ export class ZenView extends View {
 		this.plugin = plugin;
 	}
 
+	onunload() {
+		if (this.headerIcon) {
+			this.headerIcon.remove();
+		}
+
+		super.onunload();
+	}
+
 	onload() {
 		let zenLeaf = this.app.workspace.getLeavesOfType(VIEW_TYPE_ZEN)[0];
 
 		if (zenLeaf) {
+			this.createHeaderIcon(zenLeaf);
+			// @ts-ignore
 			this.updateClass(zenLeaf, this.plugin.settings.enabled);
 			// @ts-ignore
 			zenLeaf.tabHeaderEl.addEventListener("click", async (e: any) => {
 				e.stopPropagation();
 				e.preventDefault();
 
-				this.plugin.settings.enabled= !this.plugin.settings.enabled;
+				this.plugin.settings.enabled = !this.plugin.settings.enabled;
 				await this.plugin.saveSettings();
 				await this.updateClass(zenLeaf, this.plugin.settings.enabled);
 			});
 		}
+
 		super.onload();
 	}
 
-	async updateClass(leaf: any, currentState: boolean): Promise<void> {
+	createHeaderIcon(zenLeaf: WorkspaceLeaf) {
+
+		let headerIcon = createEl("div", {
+			cls: "zen-header",
+			attr: {"data-zen": "icon", "aria-label": "Toggle Zen", "aria-label-position": "right"}
+		});
+
+		let headerInner = createEl("div", {cls: "zen-header-inner"});
+
 		// @ts-ignore
-		setIcon(leaf.tabHeaderInnerIconEl, this.plugin.settings.enabled? 'eye-off' : 'eye');
+		headerInner.appendChild(getIcon("eye-off"));
+		headerIcon.appendChild(headerInner);
+
+		headerInner.addEventListener("click", async () => {
+			this.plugin.settings.enabled = !this.plugin.settings.enabled;
+			await this.plugin.saveSettings();
+			await this.updateClass(zenLeaf, this.plugin.settings.enabled);
+		});
+
+		this.headerIcon = headerIcon;
+
+		// @ts-ignore
+		this.app.workspace.leftSplit.getContainer().containerEl.appendChild(headerIcon);
+	}
+
+	async updateClass(leaf: any, currentState: boolean): Promise<void> {
 
 		if (currentState) {
 			document.body.classList.add("zen-enabled");
+
+			// @ts-ignore
+			setIcon(leaf.tabHeaderInnerIconEl, this.plugin.settings.enabled? 'eye-off' : 'eye');
+
 			Object.keys(this.plugin.settings.preferences).map((key) => {
 				// @ts-ignore
-				if(this.plugin.settings.preferences[key] === true){
-					document.body.classList.add("zen-module--"+ key);
+				if (this.plugin.settings.preferences[key] === true) {
+					document.body.classList.add("zen-module--" + key);
 				}
 			})
 			await this.plugin.saveSettings();
@@ -61,6 +101,6 @@ export class ZenView extends View {
 	}
 
 	getIcon(): string {
-		return this.plugin.settings.enabled ? 'eye-off' : 'eye';
+		return this.plugin.settings.enabled ? 'eye' : 'eye-off';
 	}
 }
